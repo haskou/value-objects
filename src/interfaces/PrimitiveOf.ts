99301ddbcb616681ca3add338ed9f64530727f81
Haskō <102477627+haskou@users.noreply.github.com>
2026-09-05 08:20:45 -0700

    style(types): 🎨 Apply repository formatting

--- src/interfaces/PrimitiveOf.ts
+++ src/interfaces/PrimitiveOf.ts
@@ -1,8 +1,11 @@
 import { Primitive } from '../types/Primitive';
 
-type SerializedValue = Primitive | SerializedValue[] | {
-  [key: string]: SerializedValue;
-};
+type SerializedValue =
+  | Primitive
+  | SerializedValue[]
+  | {
+      [key: string]: SerializedValue;
+    };
 
 export type PrimitiveOf<T extends { toPrimitives(): SerializedValue }> =
   ReturnType<T['toPrimitives']>;
